c6f2bdeb0cb42ce55e9f87f70753ee390ed3b9d2
christiano39 <carneson39@gmail.com>
2020-08-07 10:06:01 -0600

    added all routes for actions

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 
 const projectsRoutes = require('./routes/projectsRoutes');
+const actionsRoutes = require('./routes/actionsRoutes');
 
 const server = express();
 const port = process.env.PORT || 8000;
@@ -10,5 +11,6 @@ server.use(express.json());
 server.use(helmet());
 
 server.use('/api/projects', projectsRoutes);
+server.use('/api/actions', actionsRoutes);
 
 server.listen(port, () => console.log("Server running..."));
--- routes/projectsRoutes.js
+++ routes/projectsRoutes.js
@@ -57,7 +57,7 @@ router.delete('/:id', validateProjectId, (req, res) => {
         })
 })
 
-//middleware
+// validation middleware
 
 function validateProjectId(req, res, next){
     Projects.get(req.params.id)
